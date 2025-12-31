670e6820312217df426b276ece046e608e7a42e6
KALIL-devs <kalilrahman055@gmail.com>
2025-12-31 14:00:14 +0530

    favicon changed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -32,9 +32,9 @@ const Footer = () => {
 
                 <div className={styles.column}>
                     <h3>Contact</h3>
-                    <p>123 Business Avenue, Tech City</p>
-                    <p>Email: hello@agency.com</p>
-                    <p>Phone: +91 98765 43210</p>
+                    {/* <p>54/A, Chinnakadai Street, Muthaiahpillay Lane, Madurai, India, Tamil Nadu</p> */}
+                    <p>Email: fixyads@gmail.com</p>
+                    <p>Phone: +91 93613 65680</p>
                     <div style={{display: 'flex', flexDirection: 'row'}}>
                         <a href="https://www.instagram.com/fixyads/">
                             <img src="/SocialMedia/instagram.png" alt="" style={{height:'50px', width:'50px'}}/>
